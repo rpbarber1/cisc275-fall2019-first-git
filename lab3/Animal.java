cab2b429727f4664ba0736d306b276630ac7f1ed
Ryan Barber <rpbarber@udel.edu>
2019-09-10 20:05:18 -0400

    adding lab3 java files

---
+++ lab3/Animal.java
@@ -0,0 +1,13 @@
+package lab3;
+
+public class Animal {
+	String name;
+	int legs;
+	
+	public int getLegs() {
+		return legs;
+	}
+	
+	
+
+}
---
+++ lab3/Dog.java
@@ -0,0 +1,12 @@
+package lab3;
+
+public class Dog extends Animal {
+
+	
+	
+	public Dog(String n, int l) {
+		name = n;
+		legs = l;
+		
+	}
+}
